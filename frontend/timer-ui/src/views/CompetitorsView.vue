e211256ac08e12a9ab0f9ac7c16ec616233a886f
StefanA <stefanachmueller@hotmail.com>
2023-11-10 11:21:39 +0100

    Add datepicker

--- frontend/timer-ui/src/views/CompetitorsView.vue
+++ frontend/timer-ui/src/views/CompetitorsView.vue
@@ -97,10 +97,34 @@
                       sm="6"
                       md="4"
                   >
-                    <v-text-field
+                    <v-menu
+                        ref="menu"
+                        v-model="menu"
+                        :close-on-content-click="false"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                    >
+
+                    <template v-slot:activator="{ on, attrs }">
+
+                      <v-text-field
+                          v-model="editedItem.dateOfBirth"
+                          label="Date of Birth"
+                          v-bind="attrs"
+                          v-on="on"
+                      ></v-text-field>
+
+                    </template>
+
+                    <v-date-picker
                         v-model="editedItem.dateOfBirth"
                         label="Date of Birth"
-                    ></v-text-field>
+                        :active-picker.sync="activePicker"
+                    ></v-date-picker>
+
+                      </v-menu>
+
                   </v-col>
 
                   <v-col
@@ -209,6 +233,8 @@ export default {
     defaultItem: {
       name: '',
     },
+    menu: false,
+    activePicker: null,
   }),
 
   computed: {
